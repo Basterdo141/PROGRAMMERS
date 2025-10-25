332d354250153b65dd845679d7287ef30959e339
Basterdo141 <a1i7am71@gmail.com>
2025-10-25 08:11:23 -0700

    Create character_string_sentence.c

---
+++ character_string_sentence.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    char a='A';
+    char s[]="Hello";
+    char sen[]="Welcome everyone!";
+    printf("Character:%c\n",a);
+    printf("STring:%s\n",s);
+    printf("Sentence:%s\n",sen);
+    return 0;
+}
