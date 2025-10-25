833f7526ac2c5f9b27aa1f5da5912765eaeb6c29
Basterdo141 <a1i7am71@gmail.com>
2025-10-25 08:12:54 -0700

    Create sum_difference.c

---
+++ sum_difference.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n1=4;
+    int n2=3;
+    float n3=5.0;
+    float n4=2.0;
+    printf("Sum of integers:%d\n",n1+n2);
+    printf("Difference of integers:%d\n",n1-n2);
+    printf("Sum of floats:%1.1f\n",n3+n4);
+    printf("Difference of floats:%1.1f\n",n3-n4);
+    return 0;
+}
